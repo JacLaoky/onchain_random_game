9b5df33e1782086e3322e4b9b832876dacfec3bc
gec682416 <gec682416@gmail.com>
2026-02-10 03:40:54 +0800

    Delete contracts/src directory

--- contracts/src/interfaces/IRandomnessConsumer.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IRandomnessConsumer {
-    function rawFulfillRandomness(uint256 requestId, uint256[] calldata randomWords) external;
-}
