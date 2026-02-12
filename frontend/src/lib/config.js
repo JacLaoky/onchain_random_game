b8492ee810825a24a928813d5a1e2d671dd5a26a
JacLaoky <lci1373@hotmail.com>
2026-02-12 13:18:02 +0800

    Add application configuration for Sepolia Testnet

---
+++ frontend/src/lib/config.js
@@ -0,0 +1,7 @@
+export const APP_CONFIG = {
+  contractAddress: "0x8fB0101A87926ba875774A498fa186dd7b52d7A1",
+  defaultToken: "0x0000000000000000000000000000000000000000",
+  chainId: 11155111,
+  networkName: "Sepolia Testnet",
+  blockExplorer: "https://sepolia.etherscan.io"
+};
